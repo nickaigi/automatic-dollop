9458d732c6457ee6eb0ec5fe031b05d4fe311626
Nickson Kaigi <nickaigi@gmail.com>
2020-10-14 07:28:16 +0300

    can not believe it was this simple

--- first_duplicate.py
+++ first_duplicate.py
@@ -2,18 +2,12 @@ def firstDuplicate(a):
     if len(a) == len(set(a)):
         return -1
     
-    duplicates = {}
-    for i, x in enumerate(a):
-        for j, y in enumerate(a[(i + 1):], i+1):
-            if x == y:
-                if x in duplicates:
-                    continue
-                else:
-                    duplicates[x] = j
-
-    sorted_list = [k for k,v in sorted(duplicates.items(), key=lambda item: item[1])]
-    return sorted_list[0]
-
+    seen = set()
+    for i in a:
+        if i in seen:
+            return i
+        else:
+            seen.add(i)
 
 if __name__ == '__main__':
     #a = [2, 1, 3, 5, 3, 2]
