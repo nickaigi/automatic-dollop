42b6b85d0a016b91e0f1ea48f4469395b2f978f6
Nickson Kaigi <nickaigi@gmail.com>
2020-10-17 11:33:35 +0300

    solved

--- cryptarithm.py
+++ cryptarithm.py
@@ -9,39 +9,22 @@ def isCryptSolution(crypt, solution):
     for combo in solution:
         key[combo[0]] = combo[1]
     
+    leading_zeros = False
+    if key[crypt[0][0]] == '0' or key[crypt[1][0]] == '0' or key[crypt[2][0]] == '0':
+        leading_zeros = True
+ 
     decode_one = decypher(crypt[0], key)
     decode_two = decypher(crypt[1], key)
     decode_three = decypher(crypt[2], key)
     
-    if len(decode_one) > 1 and decode_one[0] == '0':
-        return False
-        
-    if len(decode_two) > 1 and decode_two[0] == '0':
-        return False
     
     if int(decode_one) + int(decode_two) == int(decode_three):
-        return True
+        if leading_zeros and int(decode_three) == 0 and len(decode_three) == len(str(int(decode_three))):
+            return True
+        elif leading_zeros:
+            return False
+        else:
+            return True
     
-    return False
-
-
-if __name__ == '__main__':
-    crypt = ["SEND", "MORE", "MONEY"]
-    solution = [
-        ['O', '0'],
-        ['M', '1'],
-        ['Y', '2'],
-        ['E', '5'],
-        ['N', '6'],
-        ['D', '7'],
-        ['R', '8'],
-        ['S', '9']
-    ]
-
-    crypt2 = ['AA', 'AA', 'AA']
-    solution2 = [
-        ['A', '0'],
-    ]
- 
-    print(isCryptSolution(crypt, solution))
-    print(isCryptSolution(crypt2, solution2))
+    else:
+        return False
