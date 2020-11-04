1450c1c644423406f32b4a9ca9e66e357a49061f
Nickson Kaigi <nickaigi@gmail.com>
2020-11-04 20:34:48 +0300

    passes 4/8 tests

--- grouping_dishes.py
+++ grouping_dishes.py
@@ -1,13 +1,27 @@
 def grouping_dishes(dishes):
     meals = {}
+    temp_solution = []
     solution = []
     main_ingredients = []
     for dish in dishes:
         meals[dish[0]] = dish[1:]
 
     [[main_ingredients.append(ing) for ing in v if ing not in main_ingredients] for k,v in meals.items()]
+    for ing in sorted(main_ingredients, key=str.lower):
+        temp = []
+        temp.append(ing)
+        for d in dishes:
+            if ing in d[1:]:
+                temp.append(d[0])
+        if len(temp) >= 3:
+            temp_solution.append(temp)
 
-    print(main_ingredients)
+    for s in temp_solution:
+        temp = []
+        temp.append(s[0])
+        [temp.append(_) for _ in sorted(s[1:], key=str.lower)]
+        solution.append(temp)
+    return solution
 
 if __name__ == '__main__':
     dishes = [
@@ -17,4 +31,4 @@ if __name__ == '__main__':
         ['Sandwich', 'Salad', 'Bread', 'Tomato', 'Cheese']
     ]
 
-    grouping_dishes(dishes)
+    print(grouping_dishes(dishes))
