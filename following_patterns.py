6ffd06a40d2a01d68be52d194fe3d9eefbba1882
Nickson Kaigi <nickaigi@gmail.com>
2020-11-05 20:27:47 +0300

    initial import

---
+++ following_patterns.py
@@ -0,0 +1,9 @@
+def are_following_patterns(strings, patterns):
+    return False
+
+
+if __name__ == '__main__':
+    strings = ['cat', 'dog', 'dog']
+    patterns = ['a', 'b', 'b']
+
+    are_following_patterns(strings, patterns)
