5334b0ffbe58999f0f07f03cff77cd188b0fb7d7
Nickson Kaigi <nickaigi@gmail.com>
2020-11-09 21:26:14 +0300

    need to iterate over the dict.values to determine the abs difference of the indices

--- contains_close_nums.py
+++ contains_close_nums.py
@@ -1,16 +1,18 @@
 def contains_close_nums(nums, k):
-    d = {}
+    groups = {}
     for i in range(len(nums)):
-        if nums[i] in d.keys():
-            if abs(d[nums[i]] - i) <= k:
-                return True
-            else:
-                d[nums[i]] = i
-    return False
+        groups.setdefault(nums[i], []).append(i)
 
+    for l in groups.values():
+        if len(l) > 1:
+            print(l)
 
 if __name__ == '__main__':
     nums = [0, 1, 2, 3, 5, 2]
     k = 3
 
+    contains_close_nums(nums, k)
+
+    nums = [1, 0, 1, 1]
+    k = 1
     contains_close_nums(nums, k)
