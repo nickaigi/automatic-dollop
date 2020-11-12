4b12b95969418469683f3e10c68c2e605e222922
Nickson Kaigi <nickaigi@gmail.com>
2020-11-12 20:15:33 +0300

    added a concise version of solution, changed sum to sum_

--- possible_sums.py
+++ possible_sums.py
@@ -1,16 +1,23 @@
 def possibleSums(coins, quantity):
     sums = {0}
-    
     for c, q in zip(coins, quantity):
         new = {}
         for i in range(1, q+1):
-            for sum in sums:
-                temp = sum + i * c
+            for sum_ in sums:
+                temp = sum_ + i * c
                 if temp not in sums:
                     new[temp] = temp
         sums.update(new)
     
     return len(sums) - 1
+
+def possible_sums_concise(coins, quantity):
+    sums = {0}
+    for c, q in zip(coins, quantity):
+        sums = {x + i * c for x in sums for i in range(q + 1)}
+
+    return len(sums) - 1
+
 
 if __name__ == '__main__':
     coins = [10, 50, 100]
