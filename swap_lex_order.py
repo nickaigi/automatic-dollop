8e239063b190dea57d77895f51c552959850c2e2
Nickson Kaigi <nickaigi@gmail.com>
2020-11-12 07:41:30 +0300

    initial import

---
+++ swap_lex_order.py
@@ -0,0 +1,11 @@
+def swap_lex_order(str_, pairs):
+    pass
+
+
+if __name__ == '__main__':
+    str_ = 'abdc'
+    pairs = [
+        [1, 4],
+        [3, 4],
+    ]
+    print(swap_lex_order(str_, pairs))
