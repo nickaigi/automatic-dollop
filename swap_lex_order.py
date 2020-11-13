7b4cfd0ed09b08b05cce687787754002035a8c83
Nickson Kaigi <nickaigi@gmail.com>
2020-11-13 21:46:54 +0300

    there is a problem

--- swap_lex_order.py
+++ swap_lex_order.py
@@ -1,6 +1,20 @@
 def swap_lex_order(str_, pairs):
-    pass
+    d = {}
+    d[str_] = True
+    while True:
+        old_len = len(d)
+        for x, y in pairs:
+            for s in d.keys():
+                d[swap(s, x, y)] = True
+        if len(d) == old_len:
+            return sorted(d)[-1]
 
+def swap(str_, x, y):
+    x = x - 1
+    y = y - 1
+    a = str_[x]
+    b = str_[y]
+    return str_[0:x] + b + str_[x + 1: y] + a + str_[y + 1:]
 
 if __name__ == '__main__':
     str_ = 'abdc'
