40de210f3d4587e7f6965f73c80af68f2604fb31
Nickson Kaigi <nickaigi@gmail.com>
2020-11-17 19:42:40 +0300

    added preorder, inorder and postorder

--- tree_traversal.py
+++ tree_traversal.py
@@ -4,27 +4,44 @@ class Tree(object):
         self.right = None
         self.left = None
 
-
-
-def traverse(node):
-    value = 0
+def inorder(root):
+    """
+    Inorder BST traversal:
+        1. Traverse the left subtree: inorder(left)
+        2. Visit the root
+        3. Traverse the right subtree: inorder(right)
+    """
     if node:
-        pass
+        inorder(root.left)
+        print (root.value)
+        inorder(root.right)
+
+def postorder(root):
+    """
+    Postorder BST traversal:
+        1. Traverse left subtree: postorder(left)
+        2. Traverse left subtree: postorder(left)
+        3. Visit the root
+    """
+    if root:
+        postorder(root.left)
+        postorder(root.right)
+        print(root.value)
 
 
-def print_tree(node):
-    if node:
-        print(node.value)
-    if node.left:
-        print_tree(node.left)
-    if node.right:
-        print_tree(node.right)
-
-
+def preorder(root):
+    """
+    Preorder BST traversal:
+        1. visit the root
+        2. Traverse left subtree: preorder(left)
+        3. Traverse right subtree: preorder(right)
+    """
+    if root:
+        print(root.value)
+        preorder(root.left)
+        preorder(root.right)
 
 if __name__ == '__main__':
     root = Tree(4)
     root_right = Tree(3)
     root_left = Tree(1)
-
-    print_tree(root)
