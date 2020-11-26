1f930c7236fd0f41a84cfd6a4133bddc73f0f17a
Nickson Kaigi <nickaigi@gmail.com>
2020-11-26 18:20:01 +0300

    initial import

---
+++ trees/restore_binary_tree.py
@@ -0,0 +1,10 @@
+def restore_binary_tree(inorder, preorder):
+    """
+    inorder: traversal first visits the left subtree, then the root, then its
+    right subtree
+    preorder: traversal first visits the root, then its left subtree, then its
+    right subtree
+
+    Both inorder and preorder are integer arrays of the values found at the node
+    """
+    pass
