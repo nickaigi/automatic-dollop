3b9dbba18a1b6f998df061a5d69835134a88bf53
Nickson Kaigi <nickaigi@gmail.com>
2020-11-30 09:52:19 +0300

    found soln

--- arcade_solutions/almost_increasing_sequence.py
+++ arcade_solutions/almost_increasing_sequence.py
@@ -1,16 +1,19 @@
 def almost_increasing_sequence(sequence):
-    if len(sequence) == 1:
-        return True
-    fails_test = []
-    for i in range(len(sequence) - 1):
-        if sequence[i] >= sequence[i+1]:
-            fails_test.append(sequence[i])
-    if len(fails_test) <= 1:
-        return True
-    else:
-        # test if removing the single number will pass_test
-        return False
-
+    dropped = False
+    last = prev = min(sequence) - 1
+    for elm in sequence:
+        if elm <= last:
+            if dropped:
+                return False
+            else:
+                dropped = True
+            if elm <= prev:
+                prev = last
+            elif elm >= prev:
+                prev = last = elm
+        else:
+            prev, last = last, elm
+    return True
 
 if __name__ == '__main__':
     sequence = [1, 3, 2, 1]
