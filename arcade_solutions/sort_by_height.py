c636202d85c7b92c09575f69b64bf76352e30723
Nickson Kaigi <nickaigi@gmail.com>
2020-12-01 12:44:50 +0300

    solution

--- arcade_solutions/sort_by_height.py
+++ arcade_solutions/sort_by_height.py
@@ -1,6 +1,11 @@
 def sort_by_height(a):
-    pass
+    l = sorted([i for i in a if i > 0])
+    for n, i in enumerate(a):
+        if i == -1:
+            l.insert(n, i)
+    return l
 
 
 if __name__ == '__main__':
     a = [-1, 150, 190, 170, -1, -1, 160, 180]
+    sort_by_height(a)
