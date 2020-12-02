f0bcbbdc2505a7cf3bf1b3f28527af77d5e51d2a
Nickson Kaigi <nickaigi@gmail.com>
2020-12-02 20:36:49 +0300

    found the section to reverse

--- arcade_solutions/reverse_in_parentheses.py
+++ arcade_solutions/reverse_in_parentheses.py
@@ -3,6 +3,12 @@ def reverse_in_parentheses(str_):
         return ''
     str_stack = []
     reverse_stack = []
+    found_start, found_stop = False, False
     for i, c in enumerate(str_):
-        pass
-    return ''
+        if c == '(':
+            found_start = True
+        elif c == ')':
+            found_stop = True
+        if found_start and not found_stop:
+            reverse_stack.append(c)
+    print(reverse_stack[1:])
