1a5e08c7292762c099a0c8a1c308d45068690172
Nickson Kaigi <nickaigi@gmail.com>
2020-12-07 19:09:46 +0300

    added a short pythonic soln

--- arcade_solutions/palindrome_rearranging.py
+++ arcade_solutions/palindrome_rearranging.py
@@ -1,9 +1,9 @@
 def palindrome_rearranging(s):
     if s == s[::-1]:
         return True
-    unique_chars = {c for c in s}
+    unique_chars = set(list(s))
     count = []
-    count_odd, count_even = 0, 0
+    count_odd = 0
 
     for c in unique_chars:
         count.append(s.count(c))
@@ -11,12 +11,20 @@ def palindrome_rearranging(s):
     for num in count:
         if num % 2 != 0:
             count_odd += 1
-    if count_odd == 1 or count_odd == 0:
+    if count_odd <= 1:
         return True
     return False
+
+
+def palindrome_rearranging_pythonic(s):
+    l = list(s)
+    chars = set(l)
+    counts = sum([l.count(x) % 2 for x in chars])
+    return counts <= 1
 
 
 if __name__ == '__main__':
     s = 'aabb'
     print(palindrome_rearranging(s))
+    print(palindrome_rearranging_pythonic(s))
     pass
