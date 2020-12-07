89f4b7d3837681071d47f6b01a5930575d475337
Nickson Kaigi <nickaigi@gmail.com>
2020-12-07 17:03:36 +0300

    my soln works

--- arcade_solutions/array_change.py
+++ arcade_solutions/array_change.py
@@ -1,8 +1,13 @@
 def arrayChange(a):
-    count, diff = 0, 0
-    temp =[]
+    diff =[]
     for i in range(len(a) -1 ):
-        if a[i+1] < a[i]:
-            diff = a[i] - a[i+1] + 1 #!+ add the one to make a[i+1] greater by 1
-        else:
-            temp.append(a[i])
+        if a[i+1] <= a[i]:
+            d = a[i] - a[i+1] + 1 #!+ add the one to make a[i+1] greater by 1
+            diff.append(d)
+            a[i+1] += d
+    return sum(diff)
+
+
+if __name__ == '__main__':
+    a = [1, 1, 1]
+    print(arrayChange(a))
