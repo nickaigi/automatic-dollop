3d6208236644fb17d7eedda3a3dd777d78489747
Nickson Kaigi <nickaigi@gmail.com>
2020-12-07 19:01:34 +0300

    my soln

--- arcade_solutions/palindrome_rearranging.py
+++ arcade_solutions/palindrome_rearranging.py
@@ -1,4 +1,18 @@
 def palindrome_rearranging(s):
+    if s == s[::-1]:
+        return True
+    unique_chars = {c for c in s}
+    count = []
+    count_odd, count_even = 0, 0
+
+    for c in unique_chars:
+        count.append(s.count(c))
+
+    for num in count:
+        if num % 2 != 0:
+            count_odd += 1
+    if count_odd == 1 or count_odd == 0:
+        return True
     return False
 
 
