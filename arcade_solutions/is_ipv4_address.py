e8f987b5b687e4fb496879bbef218b55e1e1195f
Nickson Kaigi <nickaigi@gmail.com>
2020-12-08 19:54:25 +0300

    soln

--- arcade_solutions/is_ipv4_address.py
+++ arcade_solutions/is_ipv4_address.py
@@ -1,15 +1,12 @@
 def is_ipv4_address(s):
-    ips = inputString.split('.')
-    if len(ips) != 4:
+    ip = s.split('.')
+    if len(ip) != 4:
         return False
-    for count, ip in enumerate(ips):
-        try:
-            if int(ip) == 0 and count == 0:
-                continue
-            if ip == '' or int(ip) > 255:
-                return False
-            if ip.startswith('0') and ip != '0':
-                return False
-        except Exception:
+    for i in range(len(ip)):
+        if not ip[i].isnumeric():
+            return False
+        elif len(ip[i]) > 1 and ip[i][0] == '0':
+            return False
+        elif int(ip[i]) not in range(256):
             return False
     return True
