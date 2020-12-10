1749d44ce46a1d51b8e0f660738e02fbb9a342bd
Nickson Kaigi <nickaigi@gmail.com>
2020-12-10 22:10:22 +0300

    solves 3x3 image

--- arcade_solutions/box_blur.py
+++ arcade_solutions/box_blur.py
@@ -1,6 +1,20 @@
+def sum_sub_image(sub_image):
+    """ give a 3x3 matrix ,find the sum of all its elements
+    """
+    total = 0
+    for row in sub_image:
+        for x in row:
+            total += x
+    return total
+
+
 def box_blur(image):
     total = 0
     result = []
+    n = len(image)  #!+ we have a n by n matrix
+    if n == 3:
+        result.append([sum_sub_image(image)//9])
+
     return result
 
 if __name__ == '__main__':
@@ -10,4 +24,11 @@ if __name__ == '__main__':
         [1, 1, 1],
     ]
     print(box_blur(image))
+    image2 = [
+        [7, 4, 0, 1],
+        [5, 6, 2, 2],
+        [6, 10, 7, 8],
+        [1, 4, 2, 0],
+    ]
+    print(box_blur(image2))
 
