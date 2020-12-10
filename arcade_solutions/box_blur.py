1720ad8b709be30ee491bf231c88d65668e819a5
Nickson Kaigi <nickaigi@gmail.com>
2020-12-10 20:41:17 +0300

    initial import

---
+++ arcade_solutions/box_blur.py
@@ -0,0 +1,13 @@
+def box_blur(image):
+    total = 0
+    result = []
+    return result
+
+if __name__ == '__main__':
+    image = [
+        [1, 1, 1],
+        [1, 7, 1],
+        [1, 1, 1],
+    ]
+    print(box_blur(image))
+
