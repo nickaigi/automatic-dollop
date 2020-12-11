6e8b8d648551803f73096a0e095be73997e699c8
Nickson Kaigi <nickaigi@gmail.com>
2020-12-11 17:18:10 +0300

    soln

--- arcade_solutions/box_blur.py
+++ arcade_solutions/box_blur.py
@@ -9,21 +9,40 @@ def sum_sub_image(sub_image):
 
 
 def box_blur(image):
-    total = 0
-    result = []
+    square = []  #!+ holds 3x3 matrix, used to find its blured pixel
+    square_row = []  #!+ stores one row of a 3x3 matrix,
+                     #will be appended to square
+    blur_row = []  #!+ stores resulting blurred pixels in one row
+                   # and will be appended in the blur_img
+    blur_img = []  #!+ will hold the resulting blurred image
+
     rows = len(image)
     cols = len(image[0])
-    if rows == cols and rows == 3:
-        total = sum_sub_image(image)
-        result.append([total])
-    elif rows == cols:
-        sub_images = pow((rows - 3) + 1, 2)
-        print(rows, sub_images)
-    else:
-        # rows != cols
-        print(rows, cols)
 
-    return result
+    row_pointer, col_pointer = 0, 0
+    while row_pointer <= rows - 3:
+        while col_pointer <= cols - 3:
+            for i in range(row_pointer, row_pointer + 3):
+                for j in range(col_pointer, col_pointer + 3):
+                    square_row.append(image[i][j])
+                square.append(square_row)
+                square_row = []
+            blur_row.append(sum_sub_image(square))
+            square = []
+            col_pointer += 1
+        blur_img.append(blur_row)
+        blur_row = []
+        row_pointer += 1
+        col_pointer = 0
+    return blur_img
+
+def box_blur_short(image):
+    return[
+        [sum(sum(x[i:i+3]) for x in image[j:j+3]) / 9
+         for j in range(len(image[0]) -2)
+        ] for j in range(len(image)-2)
+    ]
+
 
 if __name__ == '__main__':
     image = [
