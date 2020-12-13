01e7861d90d40ade1bf5a60813ee1b7a42e5dea0
Nickson Kaigi <nickaigi@gmail.com>
2020-12-13 19:08:17 +0300

    Update minesweeper.py

--- arcade_solutions/minesweeper.py
+++ arcade_solutions/minesweeper.py
@@ -2,5 +2,11 @@ def minesweeper(matrix):
     rows = len(matrix)
     cols = len(matrix[0])
     for i in range(rows):
+        temp = []
         for j in range(cols):
-            print(matrix[i][j])
+            if matrix[i][j]:
+                temp.append(1)
+            else:
+                temp.append(0)
+        result.append(temp)
+    return result
