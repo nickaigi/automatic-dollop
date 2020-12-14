b993be3c4b9cde4effa115035c11f45552acc37b
Nickson Kaigi <nickaigi@gmail.com>
2020-12-14 14:44:50 +0300

    soln

--- arcade_solutions/minesweeper.py
+++ arcade_solutions/minesweeper.py
@@ -1,12 +1,38 @@
+def neighbors(matrix,i,j,row,col):
+    mine = 0
+    if not ((i < 1) or (j < 1)):
+        if matrix[i - 1][j - 1]:
+            mine+=1
+    if not (i < 1):
+        if matrix[i - 1][j]:
+            mine+=1
+    if not ((i < 1) or (j >= col-1)):
+        if matrix[i - 1][j + 1]:
+            mine+=1
+    if not (j >= col-1):
+        if matrix[i][j + 1]:
+            mine+=1
+    if not ((i >= row-1) or (j >= col-1)):
+        if matrix[i + 1][j + 1]:
+            mine+=1
+    if not (i >= row-1):
+        if matrix[i + 1][j]:
+            mine+=1
+    if not ((i >= row-1) or (j < 1)):
+        if matrix[i + 1][j - 1]:
+            mine+=1
+    if not (j < 1):
+        if matrix[i][j - 1]:
+            mine+=1
+    return mine
+
 def minesweeper(matrix):
     rows = len(matrix)
     cols = len(matrix[0])
+    result = []
     for i in range(rows):
         temp = []
         for j in range(cols):
-            if matrix[i][j]:
-                temp.append(1)
-            else:
-                temp.append(0)
+            temp.append(neighbors(matrix, i, j, rows, cols))
         result.append(temp)
     return result
