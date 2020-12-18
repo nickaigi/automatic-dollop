a2d55becb6aaa43d2f479241a9a4d26a6ef22df4
Nickson Kaigi <nickaigi@gmail.com>
2020-12-18 14:56:56 +0300

    the median minimizes the sum

--- arcade_solutions/absolute_values_sum_minimization.py
+++ arcade_solutions/absolute_values_sum_minimization.py
@@ -1,14 +1,11 @@
 def absolute_values_sum_minimization(a):
-    diffs = []
-    for i in range(len(a)):
-        x = a[i]
-        sum_i = 0
-        for val in a:
-            sum_i = sum_i + abs(x - val) 
-        diffs.append(sum_i)
-    return min(diffs)
+    i = (len(a)-1)//2
+    return a[i]
 
 
 if __name__ == '__main__':
     a = [2, 4, 7]
     print(absolute_values_sum_minimization(a))
+    print(
+        absolute_values_sum_minimization([2,3])
+    )
