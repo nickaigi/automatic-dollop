dd88a6e44d90f52709454bd06094bdf7d4b5b854
Nickson Kaigi <nickaigi@gmail.com>
2020-12-18 22:20:24 +0300

    passes 3/11 tests

--- arcade_solutions/strings_rearrangement.py
+++ arcade_solutions/strings_rearrangement.py
@@ -1,7 +1,30 @@
+import itertools
+
+
 def strings_rearrangement(a):
-    print(a)
+    for perm in itertools.permutations(a):
+        if consecutive_strings_differ_by_one_char(perm):
+            return True
     return False
 
+
+def consecutive_strings_differ_by_one_char(a):
+    iter_input_array = a[:-1]
+    for i, current in enumerate(iter_input_array):
+        next_str = a[i + 1]
+        if not differ_by_one_char(current, next_str):
+            return False
+    return True
+
+
+def differ_by_one_char(s1, s2):
+    result = False
+    for char1, char2 in zip(s1, s2):
+        if char1 != char2:
+            return False
+        else:
+            result = True
+    return result
 
 if __name__ == '__main__':
     a = ['aba', 'bbb', 'bab']
