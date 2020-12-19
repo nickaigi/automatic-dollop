64965cfb20f92d5518f6a7ae82e4d491a54d13ed
Nickson Kaigi <nickaigi@gmail.com>
2020-12-19 17:11:05 +0300

    added short version

--- arcade_solutions/strings_rearrangement.py
+++ arcade_solutions/strings_rearrangement.py
@@ -1,6 +1,17 @@
 import itertools
 
 
+def diff(s1, s2):
+    return sum([a[0] != a[1] for a in zip(s1, s2)]) == 1
+
+
+def strings_rearrangement_short(a):
+    for z in itertools.permutations(a):
+        if sum([diff(*x) for x in zip(z, z[1:])]) == len(a) - 1:
+            return True
+    return False
+
+
 def strings_rearrangement(a):
     permutations = itertools.permutations(a)
     for elem in permutations:
