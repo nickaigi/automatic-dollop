eb869025b1d60f451a9f17386a05d019f08bec92
Nickson Kaigi <nickaigi@gmail.com>
2020-12-21 19:09:10 +0300

    initial import

---
+++ arcade_solutions/knapsack_light.py
@@ -0,0 +1,11 @@
+def knapsack_light(v1, w1, v2, w2, max_weight):
+    pass
+
+
+if __name__ == '__main__':
+    value1 = 10
+    weight1 = 5
+    value2 = 6
+    weight2 = 4
+    max_weight = 8
+    knapsack_light(value1, weight1, value2, weight2, max_weight)
