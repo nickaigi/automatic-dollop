6c908894e159542fe47703702f332cb3d9a8ed65
Nickson Kaigi <nickaigi@gmail.com>
2020-12-21 18:38:53 +0300

    top voted soln

--- arcade_solutions/growing_plant.py
+++ arcade_solutions/growing_plant.py
@@ -5,3 +5,9 @@ def growing_plant(up_speed, down_speed, desired_height):
         night_height = day_height - down_speed
         day += 1
     return day
+
+
+def growing_plant_short(up_speed, down_speed, desired_height):
+    if desired_height <= up_speed:
+        return 1
+    return math.ceil((desired_height - up_speed) / (up_speed - down_speed) + 1)
