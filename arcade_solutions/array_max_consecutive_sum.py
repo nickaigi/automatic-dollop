8b0a05bcea3263ef9133073412debf0fb8d62e16
Nickson Kaigi <nickaigi@gmail.com>
2020-12-21 16:21:19 +0300

    added efficient soln

--- arcade_solutions/array_max_consecutive_sum.py
+++ arcade_solutions/array_max_consecutive_sum.py
@@ -1,9 +1,18 @@
-def array_max_consecutive_sum(arr, k):
+def array_max_consecutive_sum_short(a, k):
+    c = m = sum(a[:k])
+    for i in range(len(a) - k):
+        c = c + a[i + k] - a[i]
+        m = max(c, m)
+    return m
+
+
+def array_max_consecutive_sum(a, k):
+    # works, but has O(n^2) time which is undesirable
     result_array = []
-    for i in range(len(arr) - (k-1)):
+    for i in range(len(a) - (k-1)):
         temp = []
         for j in range(i, i+k):
-            temp.append((arr[j]))
+            temp.append((a[j]))
         result_array.append(sum(temp))
     return max(result_array)
 
