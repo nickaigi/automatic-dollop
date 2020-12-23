8d03cb3a7c0950cf82b053a503a5015b394e2b14
Nickson Kaigi <nickaigi@gmail.com>
2020-12-23 16:50:30 +0300

    passes tests but should fail

--- arcade_solutions/bishop_and_pawn.py
+++ arcade_solutions/bishop_and_pawn.py
@@ -1,13 +1,13 @@
 def get_color(col, row):
     file_one= {
-        'A': 'black',
-        'B': 'white',
-        'C': 'black',
-        'D': 'white',
-        'E': 'black',
-        'F': 'white',
-        'G': 'black',
-        'H': 'white'
+        'a': 'black',
+        'b': 'white',
+        'c': 'black',
+        'd': 'white',
+        'e': 'black',
+        'f': 'white',
+        'g': 'black',
+        'h': 'white'
     }
     if row % 2 == 0:
         if file_one[col] == 'black':
@@ -20,9 +20,15 @@ def get_color(col, row):
 
 
 def bishopAndPawn(bishop, pawn):
-    b0, b1 = bishop[0], bishop[1]
-    p0, p1 = pawn[0], pawn[1]
+    b0, b1 = bishop[0], int(bishop[1])
+    p0, p1 = pawn[0], int(pawn[1])
     
+    bishop_color = get_color(b0, b1)
+    pawn_color = get_color(p0, p1)
+
+    if bishop_color != pawn_color:
+        return False
+
     if b0 == p0 or b1 == p1:
         # same row or same column
         return False
