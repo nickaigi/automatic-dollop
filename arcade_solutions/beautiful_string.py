54534f56ac91607a30ddda2602a42ccd54eb8d52
Nickson Kaigi <nickaigi@gmail.com>
2020-12-26 08:45:17 +0300

    fails with s = bbc

--- arcade_solutions/beautiful_string.py
+++ arcade_solutions/beautiful_string.py
@@ -5,9 +5,18 @@ ALPHABET = string.ascii_lowercase
 
 
 def is_beautiful_string(s):
-    pass
+    counts = []
+    for ch in ALPHABET:
+        if ch in s:
+            counts.append(s.count(ch))
+    for i in range(len(counts) - 1):
+        if counts[i] < counts[i+1]:
+            return False
+    return True
 
 
 if __name__ == '__main__':
     s = 'bbbaacdafe'
-    is_beautiful_string(s)
+    print(is_beautiful_string(s))
+    s = 'bbc'
+    print(is_beautiful_string(s))  # should print False, because there are more b's than 'a'
