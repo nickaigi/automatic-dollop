62fee13887d4bff8eda027fabde06af40a7fbcbb
Nickson Kaigi <nickaigi@gmail.com>
2020-12-27 12:03:35 +0300

    soln

--- arcade_solutions/build_palindrome.py
+++ arcade_solutions/build_palindrome.py
@@ -1,11 +1,22 @@
 def build_palindrome(st):
-    half = len(st) // 2
-    if st != st[::-1]:
-        new_st = st + st[:half][::-1]
-        return new_st
-    return st
+    for i in range(0, len(st)):
+        if st[i:len(st)] == st[i:len(st)][::-1]:
+            return st[0:i] + st[i:len(st)] + st[0:i][::-1]
 
+
+
+def build_palindrome_two(st):
+    string_list = list(st)
+    i = 0
+    while string_list != string_list[::-1]:
+        string_list.insert(len(st), st[i])
+        i += 1
+    return ''.join(string_list)
 
 if __name__ == '__main__':
     st = 'abcdc'
-    print(build_palindrome(st))
+    print(f'{st} -> {build_palindrome_two(st)}')
+    st = 'ababab'
+    print(f'{st} -> {build_palindrome_two(st)}')
+    st = 'abc'
+    print(f'{st} -> {build_palindrome_two(st)}')
