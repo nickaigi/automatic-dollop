58726f2f382ccbf86856ff861d4c60946eaad4f4
Nickson Kaigi <nickaigi@gmail.com>
2020-12-27 09:13:59 +0300

    passes 6/11 tets

--- arcade_solutions/build_palindrome.py
+++ arcade_solutions/build_palindrome.py
@@ -1,6 +1,11 @@
 def build_palindrome(st):
     half = len(st) // 2
     if st != st[::-1]:
-        new_st = st + st[:half + 1][::-1]
+        new_st = st + st[:half][::-1]
         return new_st
     return st
+
+
+if __name__ == '__main__':
+    st = 'abcdc'
+    print(build_palindrome(st))
