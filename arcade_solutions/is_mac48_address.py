a08672ddafdaea665721ab8fbc4636e795f0062b
Nickson Kaigi <nickaigi@gmail.com>
2020-12-28 21:23:26 +0300

    passes 17/20

--- arcade_solutions/is_mac48_address.py
+++ arcade_solutions/is_mac48_address.py
@@ -1,5 +1,17 @@
+import string
+
+
 def is_mac48_address(s):
     address = s.split('-')
     if len(address) != 6:
         return False
+    for grp in address:
+        for ch in grp:
+            if ch not in string.hexdigits:
+                return False
     return True
+
+
+if __name__ == '__main__':
+    s = '00-1B-63-84-45-E6'
+    is_mac48_address(s)
