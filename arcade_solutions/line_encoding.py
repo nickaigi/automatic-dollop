1e6d107ea2b5deabf1f612d1369b7dc710b518c0
Nickson Kaigi <nickaigi@gmail.com>
2020-12-29 20:19:59 +0300

    soln and short soln

--- arcade_solutions/line_encoding.py
+++ arcade_solutions/line_encoding.py
@@ -1,15 +1,36 @@
-def line_encoding(s):
-    chars = set(s)
-    result = []
-    for c in chars:
-        num_c = s.count(c)
-        if num_c > 1:
-            result.append(f'{num_c}{c}')
+from itertools import groupby
+
+
+def line_encoding_short(s):
+    soln = ''
+    for k, g in groupby(s):
+        y = len(list(g))
+        if y == 1:
+            soln += k
         else:
-            result.append(c)
+            soln += str(y) + k
+    return soln
+
+
+def line_encoding(s):
+    count = 1
+    result = []
+    for char in range(1, len(s)):
+        if s[char] == s[char - 1]:
+            count += 1
+        else:
+            if count > 1:
+                result.append(str(count) + s[char - 1])
+            else:
+                result.append(s[char - 1])
+            count = 1
+    if s[len(s) - 1] == s[len(s) - 2]:
+        result.append(str(count) + s[len(s) -1])
+    else:
+        result.append(s[len(s) - 1])
     return ''.join(result)
 
 
 if __name__ == '__main__':
     s = 'aabbbc'
-    print(line_encoding(s))
+    print(line_encoding_short(s))
