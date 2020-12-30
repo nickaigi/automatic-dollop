864cd78aee7c2fb745bc1001186526a09ee4b472
Nickson Kaigi <nickaigi@gmail.com>
2020-12-30 11:39:31 +0300

    planning

--- arcade_solutions/chess_knight.py
+++ arcade_solutions/chess_knight.py
@@ -1,3 +1,15 @@
+COLS = ['a', 'b', 'c', 'd', 'f', 'g', 'h']
+ROWS = [1, 2, 3, 4, 5, 6, 7, 8]
+
+
+def can_move(direction,side, cell):
+    """
+    direction - up/down
+    side - right, left
+    """
+    return False
+
+
 def chess_knight(cell):
     pass
 
