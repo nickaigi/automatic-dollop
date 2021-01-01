b75a4678c9ad578a0db515daa53ac2c7cfe6ac34
Nickson Kaigi <nickaigi@gmail.com>
2021-01-01 11:35:38 +0300

    almost there

--- arcade_solutions/delete_digit.py
+++ arcade_solutions/delete_digit.py
@@ -1,8 +1,14 @@
 def delete_digit(n):
-    pass
+    digits = str(n)
+    for i,c in enumerate(digits):
+        if i == 0:
+            print(digits[1: len(digits)])
+        else:
+            print(digits[0: i], digits[i+1: len(digits)]
 
 
 if __name__ == '__main__':
     n = 152
-    soln = delete_digit(n)
-    print(soln)
+    print(delete_digit(n))
+    n = 1001
+    print(delete_digit(n))
