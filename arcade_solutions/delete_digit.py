075d9116664fa3876aee89bbfa62b0887167a578
Nickson Kaigi <nickaigi@gmail.com>
2021-01-02 14:23:56 +0300

    array splicing, made it more pythonic

--- arcade_solutions/delete_digit.py
+++ arcade_solutions/delete_digit.py
@@ -2,10 +2,10 @@ def delete_digit(n):
     results = []
     digits = str(n)
     for i,c in enumerate(digits):
-        if i == 0:
-            results.append(int(digits[1: len(digits)]))
-        else:
-            results.append(int(f'{digits[0: i]}{digits[i+1: len(digits)]}'))
+        #if i == 0:
+        #    results.append(int(digits[1:]))
+        #else:
+        results.append(int(f'{digits[:i]}{digits[i+1:]}'))
     return max(results)
 
 
