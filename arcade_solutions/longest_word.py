72136ce7a69002178def7249afc11761941a731a
Nickson Kaigi <nickaigi@gmail.com>
2021-01-02 09:41:05 +0300

    working on it

--- arcade_solutions/longest_word.py
+++ arcade_solutions/longest_word.py
@@ -1,5 +1,16 @@
+import string
+
+ALPHABET =  string.ascii_lowercase
+
+
 def longest_word(text):
-    pass
+    words = text.split(' ')
+    if len(words) == 1:
+        return text
+    for w in words:
+        for ch in w:
+            # we need an index
+            pass
 
 
 if __name__ == '__main__':
