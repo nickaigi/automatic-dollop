3732c62dfd7cfa1a836258d54215ffdef151ace6
Nickson Kaigi <nickaigi@gmail.com>
2021-01-02 14:14:07 +0300

    i have an idea

--- arcade_solutions/longest_word.py
+++ arcade_solutions/longest_word.py
@@ -5,12 +5,18 @@ ALPHABET =  string.ascii_lowercase
 
 def longest_word(text):
     words = text.split(' ')
+    candidates = []
+    longest = ''
     if len(words) == 1:
         return text
     for w in words:
-        for ch in w:
-            # we need an index
-            pass
+        if w.isaplpha():
+            for i, ch in enumerate(w):
+                j = ALPHABET.index(ch.lower())
+                if w[i + 1] == ALPHABET[j+1]:
+                    if w not in candidates:
+                        candidates.append(w)
+    return max(len(w) for w in candidates)
 
 
 if __name__ == '__main__':
