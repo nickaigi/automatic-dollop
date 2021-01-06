c18e8969d1e13cfa0650052ce67654200596469b
Nickson Kaigi <nickaigi@gmail.com>
2021-01-06 11:58:38 +0300

    added another soln

--- arcade_solutions/digits_product.py
+++ arcade_solutions/digits_product.py
@@ -1,3 +1,20 @@
+def digits_product_other(product):
+    if product == 0:
+        return 10
+    if product == 1:
+        return 1
+    n = []
+    while 1 < product:
+        for d in range(9, 1, -1):
+            if product % d == 0:
+                product /= d
+                n.append(d)
+                break
+        else:
+            return -1
+    return int(''.join(map(str, sorted(n))))
+
+
 def digits_product(product):
     for i in range(1, 10000):
         number = str(i)
@@ -12,3 +29,4 @@ def digits_product(product):
 if __name__ == '__main__':
     product = 12
     print(digits_product(product))
+    print(digits_product_other(19))
