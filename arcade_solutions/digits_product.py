2ddab83c0c1785c7d4a3151df306240869c2351e
Nickson Kaigi <nickaigi@gmail.com>
2021-01-06 11:53:14 +0300

    soln

--- arcade_solutions/digits_product.py
+++ arcade_solutions/digits_product.py
@@ -1,10 +1,14 @@
-def digits_product(number):
-    if product > 1:
-        for i in range(2, number):
-            if product % i == 0:
-                # number is not prime
-                pass
-                break
-        else:
-            # number is prime
-            return -1
+def digits_product(product):
+    for i in range(1, 10000):
+        number = str(i)
+        total = 1
+        for char in number:
+            total *= int(char)
+        if total == product:
+            return i
+    return -1
+
+
+if __name__ == '__main__':
+    product = 12
+    print(digits_product(product))
