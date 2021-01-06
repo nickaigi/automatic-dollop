16a6e201930f5c15ca063d60020087bae02c00ac
Nickson Kaigi <nickaigi@gmail.com>
2021-01-06 19:18:21 +0300

    passes 4/6

--- arcade_solutions/file_naming.py
+++ arcade_solutions/file_naming.py
@@ -1,20 +1,19 @@
 def file_naming(names):
     result = []
     for name in names:
-        if name in result:
-            new_name = name + '(1)'
-            if new_name in result:
-                count = 0
-                new_name = name
-                while new_name in result:
-                    count += 1
-                    new_name = new_name + f'({count})'
-            result.append(new_name)
-        else:
+        if name not in result:
             result.append(name)
+        else:
+            count = 1
+            new_name = name
+            while new_name in result:
+                new_name = name + f'({count})'
+                count += count
+            result.append(new_name)
     return result
 
 
 if __name__ == '__main__':
     names = ['doc', 'doc', 'image', 'doc(1)', 'doc']
+    print(names)
     print(file_naming(names))
