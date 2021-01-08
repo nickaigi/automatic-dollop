ea52b2439e5f07a8532953dbec07652bcdb66a9d
Nickson Kaigi <nickaigi@gmail.com>
2021-01-08 21:12:41 +0300

    soln

--- arcade_solutions/spiral_numbers.py
+++ arcade_solutions/spiral_numbers.py
@@ -1,15 +1,15 @@
 def spiral_numbers(n):
-    matrix = []
-    count = 1
-    for i in range(n):
-        row = []
-        for j in range(n):
-            row.append(count)
-            count += 1
-        matrix.append(row)
-    print(matrix)
-
+    arr = [[0] * n for i in range(n)]
+    dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
+    x, y, c = 0, -1, 1
+    for i in range(n + n -1):
+        for j in range((n + n - i) // 2):
+            x += dx[i % 4]
+            y += dy[i % 4]
+            arr[x][y] = c
+            c += 1
+    return arr
 
 if __name__ == '__main__':
     n = 3
-    spiral_numbers(n)
+    print(spiral_numbers(n))
