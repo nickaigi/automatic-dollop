f7562c652be762898e5536bc6e1afdd5dc7d81df
Nickson Kaigi <nickaigi@gmail.com>
2021-01-09 10:12:41 +0300

    soln

--- arcade_solutions/sudoku.py
+++ arcade_solutions/sudoku.py
@@ -1,5 +1,36 @@
+def check_numbers(numbers):
+    seen = set()
+    for num in numbers:
+        if num == '.':
+            continue
+        if num in seen:
+            return False
+        seen.add(num)
+    return True
+
+def check_sub_grids(grid):
+    for row in range(0, 9, 3):
+        for col in range(0, 9, 3):
+            if not check_numbers(
+                grid[row][col:col+3] + grid[row+1][col:col +3] + grid[row+2][col:col+3]
+            ):
+                return False
+    return True
+
+
 def sudoku(grid):
-    return False
+    for row in grid:
+        if not check_numbers(row):
+            return False
+
+    for col in range(9):
+        if not check_numbers([row[col] for row in grid]):
+            return False
+
+    if not check_sub_grids(grid):
+        return False
+    return True
+
 
 
 if __name__ == '__main__':
