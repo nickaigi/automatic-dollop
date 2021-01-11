09692e18c3a298289b0ecb85007a8a540d1d18da
Nickson Kaigi <nickaigi@gmail.com>
2021-01-11 10:32:34 +0300

    initial import

---
+++ arcade_solutions/the_core/seats_in_theater.py
@@ -0,0 +1,10 @@
+def seats_in_theater(nCols, nRows, col, row):
+    pass
+
+
+if __name__ == '__main__':
+    nCols = 16
+    nRows = 11
+    col = 5
+    row = 3
+    seats_in_theater(nCols, nRows, col, row)
