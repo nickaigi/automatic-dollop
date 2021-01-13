a7576df9f430fb4792f2e7455f9c0eef34f3aab2
Nickson Kaigi <nickaigi@gmail.com>
2021-01-13 15:45:47 +0300

    passes 5/6 tests

--- arcade_solutions/the_core/phone_call.py
+++ arcade_solutions/the_core/phone_call.py
@@ -1,5 +1,17 @@
-def phone_call(min1, min2_10, min11, s):
-    pass
+def phone_call(min1, min2_10, min11, bal):
+    minutes = 0
+    if bal >= min1:
+        minutes += 1
+        bal -= min1
+    if bal >= min2_10 * 9:
+        bal -= min2_10 * 9
+        minutes += 9
+    else:
+        minutes += (bal // min2_10)
+
+    if bal > min11:
+        minutes += (bal // min11)
+    return minutes
 
 
 if __name__ == '__main__':
