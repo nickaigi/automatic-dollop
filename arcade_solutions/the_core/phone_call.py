9f2d77e591ee1e732ad04f1554b8e327f68a4d3c
Nickson Kaigi <nickaigi@gmail.com>
2021-01-13 15:18:08 +0300

    initial import

---
+++ arcade_solutions/the_core/phone_call.py
@@ -0,0 +1,10 @@
+def phone_call(min1, min2_10, min11, s):
+    pass
+
+
+if __name__ == '__main__':
+    min1 = 3
+    min2_10 = 1
+    min11 = 2
+    s = 20
+    phone_call(min1, min2_10, min11, s)
