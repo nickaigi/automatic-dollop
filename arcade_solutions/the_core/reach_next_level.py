baf4946c42f0650c8c1f533d1bf121550ceca5c6
Nickson Kaigi <nickaigi@gmail.com>
2021-01-14 11:07:43 +0300

    initial import

---
+++ arcade_solutions/the_core/reach_next_level.py
@@ -0,0 +1,9 @@
+def reach_next_level(xp, threshold, reward):
+    return False
+
+
+if __name__ == '__main__':
+    xp = 10
+    threshold = 15
+    reward = 5
+    reach_next_level(xp, threshold, reward)
