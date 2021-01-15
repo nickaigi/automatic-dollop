4ccf90ba5ebb615cf440ca55c4453973c6d34654
Nickson Kaigi <nickaigi@gmail.com>
2021-01-15 11:59:33 +0300

    using xor

--- arcade_solutions/the_core/extra_numbers.py
+++ arcade_solutions/the_core/extra_numbers.py
@@ -4,3 +4,11 @@ def extra_numbers(a, b, c):
     if a == c:
         return b
     return a
+
+
+def extra_numbers_short(a, b, c):
+    """
+    using the bitwise exclusive Or
+    a ^ b == xor(a, b)
+    """
+    return a ^ b ^ c
