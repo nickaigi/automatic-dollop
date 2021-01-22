c85537f05a80effca5f1a3cc3b467ad3bdc79a90
Nickson Kaigi <nickaigi@gmail.com>
2021-01-22 18:07:13 +0300

    short soln

--- arcade_solutions/the_core/kill_kth_bit.py
+++ arcade_solutions/the_core/kill_kth_bit.py
@@ -1,15 +1,20 @@
-def kill_kth_bit(n,k):
-    bin_n = str(bin(n)[2:])
-    result = ''
-    for i in range(len(bin_n)):
-        if i == k:
-            result += '0'
-        else:
-            result += bin_n[i]
+def kill_kth_bit(n, k):
+    """
+    use bitwise AND
+        0 & 0 = 0
+        1 & 0 = 0
 
-    print(int(result, 2))
+    to clear the bit: perform a bitwise AND of the number with a reset bit
+    n = n & ~(1 << k)
+
+    OR
+
+    n &= ~(1 << k)
+    """
+    return n & ~(1 << k - 1)
+
 
 if __name__ == '__main__':
     n = 37
     k = 3
-    kill_kth_bit(n, k)
+    print(kill_kth_bit(n, k))
