3e2766f3138155667b246f083ba3d394a301ccc2
Nickson Kaigi <nickaigi@gmail.com>
2021-01-22 11:47:01 +0300

    my soln

--- arcade_solutions/the_core/metro_card.py
+++ arcade_solutions/the_core/metro_card.py
@@ -1,4 +1,7 @@
 MONTHS = [31, 28, 31, 30, 31, 30, 31, 31,30, 31, 30, 31]
 def metro_card(n):
-    result = []
-    return sorted(result)
+    result = set()
+    for i in range(len(MONTHS) - 1):
+        if n == MONTHS[i]:
+            result.add(MONTHS[i + 1])
+    return sorted(list(result))
