48adfaf5d53828fe7aa500880df00bf44bd2077a
Nickson Kaigi <nickaigi@gmail.com>
2021-01-25 19:50:49 +0300

    using a list comprehension

--- arcade_solutions/the_core/range_bit_count.py
+++ arcade_solutions/the_core/range_bit_count.py
@@ -1,4 +1,10 @@
 def range_bit_count(a, b):
-    numbers = range(a, b + 1)
-    pass
+   numbers = [i for i in range(a, b + 1)]
+   print(numbers)
+
+
+if __name__ == '__main__':
+    a = 2
+    b = 7
+    range_bit_count(a, b)
 
