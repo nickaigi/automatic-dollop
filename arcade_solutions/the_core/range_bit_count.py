9d0413a15eb999d587dcc4afecd31294c58dab0b
Nickson Kaigi <nickaigi@gmail.com>
2021-01-26 19:46:27 +0300

    soln

--- arcade_solutions/the_core/range_bit_count.py
+++ arcade_solutions/the_core/range_bit_count.py
@@ -1,7 +1,5 @@
 def range_bit_count(a, b):
-   numbers = [i for i in range(a, b + 1)]
-   print(numbers)
-
+    return sum(bin(x).count('1') for x in range(a, b+1))
 
 if __name__ == '__main__':
     a = 2
