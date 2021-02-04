e50b80a6392f612618f13662775b891a132f457c
Nickson Kaigi <nickaigi@gmail.com>
2021-02-04 20:19:01 +0300

    short version

--- arcade_solutions/the_core/least_factorial.py
+++ arcade_solutions/the_core/least_factorial.py
@@ -1,14 +1,8 @@
-def fact(n):
-    if n == 0:
-        return 1
-    return n * fact(n - 1)
-
-
 def least_factorial(n):
     k = 1
     loop = 1
     while k < n:
-        k = fact(loop)
+        k *= loop
         loop += 1
     return k
 
