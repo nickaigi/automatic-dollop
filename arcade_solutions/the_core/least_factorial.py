f5e46c986aba44fb00a500abe7fb4a6c4a9528df
Nickson Kaigi <nickaigi@gmail.com>
2021-02-04 20:13:19 +0300

    my soln

--- arcade_solutions/the_core/least_factorial.py
+++ arcade_solutions/the_core/least_factorial.py
@@ -1,3 +1,18 @@
+def fact(n):
+    if n == 0:
+        return 1
+    return n * fact(n - 1)
+
+
 def least_factorial(n):
     k = 1
+    loop = 1
+    while k < n:
+        k = fact(loop)
+        loop += 1
     return k
+
+
+if __name__ == '__main__':
+    n = 17
+    print(least_factorial(n))
