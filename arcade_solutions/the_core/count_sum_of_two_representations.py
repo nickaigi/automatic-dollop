d25c0e05887e2dedfc2e191d97347a6c5277115e
Nickson Kaigi <nickaigi@gmail.com>
2021-02-06 21:05:43 +0300

    init import

--- arcade_solutions/the_core/count_sum_of_two_representations.py
+++ arcade_solutions/the_core/count_sum_of_two_representations.py
@@ -1,2 +1,7 @@
 def count_sum_of_two_representations(n, l, r):
-    pass
+    if (2 * r < n) or (2 * l > n):
+        return 0
+    _min = max(l, n - r)
+    _max = min(r, n - l)
+    mid = (_max + _min) // 2
+    return mid - _min + 1
