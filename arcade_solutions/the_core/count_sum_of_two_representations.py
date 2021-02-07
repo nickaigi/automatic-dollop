0110441981e0d0b99f0b1cf26078df8a3b9d8236
Nickson Kaigi <nickaigi@gmail.com>
2021-02-07 09:34:01 +0300

    short soln

--- arcade_solutions/the_core/count_sum_of_two_representations.py
+++ arcade_solutions/the_core/count_sum_of_two_representations.py
@@ -5,3 +5,14 @@ def count_sum_of_two_representations(n, l, r):
     _max = min(r, n - l)
     mid = (_max + _min) // 2
     return mid - _min + 1
+
+
+def count_sum_of_two_representations_short(n, l, r):
+    return sum(1 for a in range(l, r+1) if l <= a <= n - a <= r)
+
+
+if __name__ == '__main__':
+    n = 6
+    l = 2
+    r = 4
+    count_sum_of_two_representations_short(n, l, r)
