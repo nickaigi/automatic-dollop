770b9f6df9b927ce8221b878e39ec47399f048e9
Nickson Kaigi <nickaigi@gmail.com>
2021-02-10 18:32:12 +0300

    soln

--- arcade_solutions/the_core/addition_without_carrying.py
+++ arcade_solutions/the_core/addition_without_carrying.py
@@ -1,7 +1,12 @@
 def addition_without_carrying(a, b):
-    for x, y in zip(str(a), str(b)):
-        print(x, y)
-    return 0
+    result = 0
+    ten_power = 1
+    while a > 0 or b > 0:
+        result += ten_power * ((a + b) % 10)
+        a //= 10
+        b //= 10
+        ten_power *= 10
+    return result
 
 
 if __name__ == '__main__':
