e7a27d67c59738171725df3fce872879923001d3
Nickson Kaigi <nickaigi@gmail.com>
2021-02-17 20:26:00 +0300

    soln

--- arcade_solutions/the_core/candles.py
+++ arcade_solutions/the_core/candles.py
@@ -1,7 +1,19 @@
 def candles(candles_number, make_new):
-    pass
+    total = 0
+    left_over = 0
+    while candles_number > 0:
+        total += candles_number
+        left_over += candles_number
+        candles_number = left_over // make_new
+        left_over = left_over % make_new
+    return total
+
+
+def candles_short(candles_number, make_new):
+    return candles_number +(candles_number - 1) // (make_new - 1)
+
 
 if __name__ == '__main__':
     candles_number = 5
     make_new = 2
-    candles(candles_number, make_new)
+    candles_short(candles_number, make_new)
