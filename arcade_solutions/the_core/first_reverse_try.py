323016e2aa3cb87ca04e4293bcfa2d48218c1322
Nickson Kaigi <nickaigi@gmail.com>
2021-02-23 20:08:14 +0300

    my soln

--- arcade_solutions/the_core/first_reverse_try.py
+++ arcade_solutions/the_core/first_reverse_try.py
@@ -1,2 +1,14 @@
 def first_reverse_try(arr):
-    return arr
+    result = []
+    if arr:
+        l = len(arr)
+        first = arr[0]
+        last = arr[l - 1]
+        for i in range(l):
+            if i == 0:
+                result.append(last)
+            elif i == l - 1:
+                result.append(first)
+            else:
+                result.append(arr[i])
+    return result
