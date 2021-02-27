bc5568939378212bb7e773abc30c380d9161ed3d
Nickson Kaigi <nickaigi@gmail.com>
2021-02-27 10:24:03 +0300

    my soln

--- arcade_solutions/the_core/replace_middle.py
+++ arcade_solutions/the_core/replace_middle.py
@@ -1,9 +1,17 @@
 def replace_middle(arr):
     arr_len = len(arr)
     if arr_len % 2 == 0:
+        result = []
         mid_one = arr_len // 2 - 1
         mid_two = mid_one + 1
-        print(arr, mid_one, mid_two)
+        for i in range(arr_len):
+            if i == mid_one:
+                result.append(arr[mid_one] + arr[mid_two])
+            elif i == mid_two:
+                continue
+            else:
+                result.append(arr[i])
+        return result
     return arr
 
 
