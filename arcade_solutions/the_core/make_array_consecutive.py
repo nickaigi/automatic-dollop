64cbe268290e45abf0bd8d353bfe8a055e80a43c
Nickson Kaigi <nickaigi@gmail.com>
2021-02-28 12:17:12 +0300

    init import

---
+++ arcade_solutions/the_core/make_array_consecutive.py
@@ -0,0 +1,9 @@
+def make_array_consecutive(arr):
+    arr.sort()
+    start = arr[0]
+    stop = arr[-1]
+    count = 0
+    for i in range(start, stop):
+        if i not arr:
+            count += 1
+    return count
