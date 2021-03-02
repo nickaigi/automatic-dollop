4da4311652a348239b6047bfc7cf5a77240af892
Nickson Kaigi <nickaigi@gmail.com>
2021-03-02 11:02:37 +0300

    trying

--- arcade_solutions/the_core/is_sum_of_consecutive.py
+++ arcade_solutions/the_core/is_sum_of_consecutive.py
@@ -1,5 +1,17 @@
 def is_sum_of_consecutive_2(n):
-    for i in range(n + 1):
-        for j in range(i):
-          pass
-    return 0
+    count = 0
+    i = 2
+    while i < n/2:
+        if i % 2 != 0:
+            if ((n // i) + 1) * i == n and n // i > i/2:
+                count += 1
+            else:
+                if (n // i) * i != n and n // i:
+                    count += 1
+        i += 1
+    return count
+
+
+if __name__ == '__main__':
+    n = 9
+    print(is_sum_of_consecutive_2(n))
