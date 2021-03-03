4211b3ec4231e1cfa18ea5461f40627c2cc302db
Nickson Kaigi <nickaigi@gmail.com>
2021-03-03 12:58:13 +0300

    my soln

--- arcade_solutions/the_core/square_digits_sequence.py
+++ arcade_solutions/the_core/square_digits_sequence.py
@@ -1,2 +1,14 @@
 def square_digits_sequence(a0):
-    a1, a2 = str(a0)[0], str(a0)[1]
+    result = [a0]
+    x = a0
+    while True:
+        sequence = []
+        for _ in list(str(x)):
+            sequence.append(int(_) ** 2)
+            x = sum(sequence)
+        if x in result:
+            result.append(x)
+            break
+        else:
+            result.append(x)
+    return len(result)
