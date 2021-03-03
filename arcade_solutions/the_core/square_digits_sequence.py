87e81cc29906ccf11a64a730546a6700fa3bf516
Nickson Kaigi <nickaigi@gmail.com>
2021-03-03 13:13:45 +0300

    other soln

--- arcade_solutions/the_core/square_digits_sequence.py
+++ arcade_solutions/the_core/square_digits_sequence.py
@@ -12,3 +12,23 @@ def square_digits_sequence(a0):
         else:
             result.append(x)
     return len(result)
+
+
+def square_digits_sequence_other(a0):
+    result = []
+    x = a0
+
+    while x not in result:
+        result.append(x)
+        count = 0
+        for _ in str(x):
+            count += int(_) ** 2
+        x = count
+    return len(result) + 1
+
+
+def square_digits_sequence_short(a0):
+    seq = [a0]
+    while seq[-1] not in seq[:-1]:
+        seq.append(sum(int(i) ** 2 for i in str(seq[-1])))
+    return len(seq)
