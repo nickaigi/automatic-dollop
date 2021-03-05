ee062385ad2ec993abc2a6381a89341a01979f52
Nickson Kaigi <nickaigi@gmail.com>
2021-03-05 19:02:16 +0300

    soln

--- arcade_solutions/the_core/pages_numbering_with_ink.py
+++ arcade_solutions/the_core/pages_numbering_with_ink.py
@@ -1,8 +1,15 @@
+import math
+
+
 def pages_numbering_with_ink(current, num_of_digits):
-    remaining = num_of_digits
-    current -= 1  # we have to print the current page
-    while remaining > 0:
+    while num_of_digits >= 0:
+        num_of_digits -= int(math.ceil(math.log(current+1, 10)))
         current += 1
-        x = len(str(current))
-        remaining -= x
-    return current
+    return current - 2
+
+
+if __name__ == '__main__':
+    current = 1
+    num_of_digits = 5
+    print(pages_numbering_with_ink(current, num_of_digits))
+
