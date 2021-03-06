69b8b5c4e5c93a1176c20c78a3fb5905440c41b8
Nickson Kaigi <nickaigi@gmail.com>
2021-03-06 11:52:42 +0300

    an attempt was made

--- arcade_solutions/the_core/comfortable_numbers.py
+++ arcade_solutions/the_core/comfortable_numbers.py
@@ -1,2 +1,16 @@
 def comfortable_numbers(l, r):
-    pass
+    pair = []
+    pair_count = 0
+    for i in range(l, r+1):
+        s = 0
+        temp = i
+        while temp:
+            s += temp % 10
+            temp //= 10
+        for j in range(i-s, i+s+1):
+            if i < j <= r:
+                pair.append([i, j])
+            if j < 1:
+                if [j, i] in pair:
+                    pair_count += 1
+    return pair_count
