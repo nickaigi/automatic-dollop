bbaf09c59d90ca7c57980232c934c4f5419f5d06
Nickson Kaigi <nickaigi@gmail.com>
2021-03-09 20:36:22 +0300

    soln

--- arcade_solutions/the_core/weak_numbers.py
+++ arcade_solutions/the_core/weak_numbers.py
@@ -1,2 +1,19 @@
 def weak_numbers(n):
-    pass
+    factors = [count_factors(n) for n in range(1, n+1)]
+    weaknesses = []
+    for num, num_factors in enumerate(factors, 1):
+        weakness = 0
+        for factor in factors[:num]:
+            if factor > num_factors:
+                weakness += 1
+        weaknesses.append(weakness)
+    weakest = max(weaknesses)
+    return [weakest, weaknesses.count(weakest)]
+
+
+def count_factors(n):
+    _factors = 0
+    for i in range(1, n+1):
+        if n % i == 0:
+            _factors += 1
+    return _factors
