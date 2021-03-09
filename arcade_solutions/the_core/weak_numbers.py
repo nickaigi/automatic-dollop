b46f20c62a84dda6f2dc0489f64cba55ac1cc756
Nickson Kaigi <nickaigi@gmail.com>
2021-03-09 20:45:02 +0300

    added the short version

--- arcade_solutions/the_core/weak_numbers.py
+++ arcade_solutions/the_core/weak_numbers.py
@@ -17,3 +17,9 @@ def count_factors(n):
         if n % i == 0:
             _factors += 1
     return _factors
+
+
+def weak_numbers_short(n):
+    d = [sum(i % j == 0 for j in range(1, i+1)) for i in range(1, n+1)]
+    w = [sum(j > m for j in d[:i]) for i, m in enumerate(d)]
+    return [max(w), w.count(max(w))]
