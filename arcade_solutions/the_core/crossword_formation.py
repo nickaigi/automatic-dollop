0363bb92eab89167b8b6971ff272d35160a3a776
Nickson Kaigi <nickaigi@gmail.com>
2021-03-11 09:27:41 +0300

    initial import

---
+++ arcade_solutions/the_core/crossword_formation.py
@@ -0,0 +1,7 @@
+def crossword_formation(words):
+    print(words)
+
+
+if __name__ == '__main__':
+    words = ['crossword', 'square', 'formation', 'something']
+    print(crossword_formation(words))
