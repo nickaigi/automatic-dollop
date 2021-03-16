49b08f292f329a71190478e1a6438b04fff8f2bb
Nickson Kaigi <nickaigi@gmail.com>
2021-03-16 18:30:43 +0300

    soln

--- arcade_solutions/the_core/is_mac_address.py
+++ arcade_solutions/the_core/is_mac_address.py
@@ -2,4 +2,6 @@ import string
 
 
 def is_mac_address(s):
-    return all(c in string.hexdigits + '-' for c in s)
+    if len(s) != 17:
+        return False
+    return all(c in string.hexdigits + '-' for c in s) and s.count('-') == 5
