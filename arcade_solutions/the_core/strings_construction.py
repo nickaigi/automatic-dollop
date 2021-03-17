a86c47ff8f00a35264b1e3b3ad5a86218713cf75
Nickson Kaigi <nickaigi@gmail.com>
2021-03-17 21:02:04 +0300

    long soln

--- arcade_solutions/the_core/strings_construction.py
+++ arcade_solutions/the_core/strings_construction.py
@@ -1,2 +1,8 @@
 def strings_construction(a, b):
     return min(b.count(c) // a.count(c) for c in a)
+
+def strings_construction_long(a, b):
+    c = set()
+    for x in set(a):
+        c.add(b.count(x) // a.count(x))
+    return min(c)
