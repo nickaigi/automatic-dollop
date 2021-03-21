71d1a3ed2bccf3bab8fb85bf759500b1b634443e
Nickson Kaigi <nickaigi@gmail.com>
2021-03-21 11:24:32 +0300

    soln

--- arcade_solutions/the_core/construct_square.py
+++ arcade_solutions/the_core/construct_square.py
@@ -1,4 +1,12 @@
 def construct_square(s):
+    p = len(s)
+    d_max = int((10**p)**.5)
+    d_min = int((10**(p-1))**.5)
+
+    for d in range(d_max, d_min -1, -1):
+        n = str(d * d)
+        if sorted(s.count(c) for c in set(s)) == sorted(n.count(c) for c in set(n)):
+            return int(n)
     return -1
 
 
