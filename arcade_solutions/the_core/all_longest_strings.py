3472e5c1cdf3037677212e6becbcc0b6baef248b
Nickson Kaigi <nickaigi@gmail.com>
2021-06-20 20:59:50 +0300

    short soln

--- arcade_solutions/the_core/all_longest_strings.py
+++ arcade_solutions/the_core/all_longest_strings.py
@@ -1,3 +1,7 @@
-def all_longest_strings(st):
+def all_longest_strings_old(st):
     l = max([len(s) for s in st])
     return [s for s in st if len(s) == l]
+
+
+def all_longest_strings(st):
+    return [s for s in st if len(s) == max(map(len, s))]
