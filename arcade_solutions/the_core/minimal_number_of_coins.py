3c7170bc074777648ce7c079a0b9f048270de429
Nickson Kaigi <nickaigi@gmail.com>
2021-06-25 12:47:13 +0300

    my soln

--- arcade_solutions/the_core/minimal_number_of_coins.py
+++ arcade_solutions/the_core/minimal_number_of_coins.py
@@ -1,6 +1,18 @@
 def minimal_number_of_coins(coins, price):
     bal = price
-    count = len(coins)
+    index = len(coins)
+    count = 0
     while bal > 0:
-        bal -= max(coins[:count])
-    return 1
+        if bal >= max(coins[:index]):
+            bal -= max(coins[:index])
+            count += 1
+        else:
+            index -= 1
+    return count
+
+
+
+if __name__ == '__main__':
+    coins = [1, 2, 10]
+    price = 28
+    print(minimal_number_of_coins(coins, price))
