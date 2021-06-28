0688d61a18632dd8ae84a09213e7658766ff6467
Nickson Kaigi <nickaigi@gmail.com>
2021-06-28 12:13:00 +0300

    initial import

---
+++ arcade_solutions/the_core/timed_reading.py
@@ -0,0 +1,8 @@
+def timed_reading(max_length, text):
+    return 0
+
+
+if __name__ == '__main__':
+    max_length = 4
+    text = "The Fox asked the stork, 'How is the soup?'"
+    print(timed_reading(max_length, text))
