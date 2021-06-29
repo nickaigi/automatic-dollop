e8422f7896a9cd2daf0600bbdd184f679e13b01f
Nickson Kaigi <nickaigi@gmail.com>
2021-06-29 11:07:16 +0300

    initial import

---
+++ arcade_solutions/the_core/integer_to_string_of_fixed_width.py
@@ -0,0 +1,8 @@
+def integer_to_string_of_fixed_width(number, width):
+    return "0"
+
+
+if __name__ == '__main__':
+    number = 1234
+    width = 2
+    print(integer_to_string_of_fixed_width(number, width))
