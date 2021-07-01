23aeaf9f85665552c98a4102031b91fbaedafa45
Nickson Kaigi <nickaigi@gmail.com>
2021-07-01 18:55:12 +0300

    maybe use regex

--- arcade_solutions/the_core/ada_number.py
+++ arcade_solutions/the_core/ada_number.py
@@ -1,5 +1,12 @@
+import re
+
+
 def ada_number(line):
-    return False
+    parts = [_ for _ in line.split('#')]
+    for s in parts:
+        if not re.match('[0-9a-fA-F]+', s):
+            return False
+    return True
 
 
 if __name__ == '__main__':
