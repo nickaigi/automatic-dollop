f99eb833884e8e999cbd9f5b4246451bce1cd0dc
Nickson Kaigi <nickaigi@gmail.com>
2021-07-03 17:53:59 +0300

    another soln

--- arcade_solutions/the_core/character_parity.py
+++ arcade_solutions/the_core/character_parity.py
@@ -1,5 +1,9 @@
-def character_parity(symbol):
-    if symbol.isdigit():
-        n = int(symbol)
+def character_parity(s):
+    return "not a digit" if not s.isdigit() else "odd" if int(s) % 2 else "even"
+
+
+def character_parity_old(s):
+    if s.isdigit():
+        n = int(s)
         return "even" if n % 2 == 0 else "odd"
     return "not a digit"
