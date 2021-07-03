2869447ada9d5de7079dfa5ed3ef1126a84f5fa7
Nickson Kaigi <nickaigi@gmail.com>
2021-07-03 17:42:25 +0300

    soln

--- arcade_solutions/the_core/three_split.py
+++ arcade_solutions/the_core/three_split.py
@@ -1,6 +1,18 @@
 def three_split(a):
-    return 0
+    t = sum(a) // 3
+    ans = 0
+    current = 0
+    first = 0
 
+    for i in range(len(a) - 1):
+        current += a[i]
+        if current == t:
+            first += 1
+        if current == 2 * t:
+            ans += first
+            if t == 0:
+                ans -= 1
+    return ans
 
 if __name__ == '__main__':
     a = [0, -1, 0, -1, 0, -1]
