442b060700c2cc2ffa7924eaedf3ec403835ad43
Nickson Kaigi <nickaigi@gmail.com>
2021-07-05 11:35:35 +0300

    initial import

---
+++ arcade_solutions/the_core/new_numeral_system.py
@@ -0,0 +1,11 @@
+import string
+
+def new_numeral_system(number):
+    alphabet = string.ascii_uppercase
+    return ["0"]
+
+
+if __name__ == '__main__':
+    number = 'G'
+    print(new_numeral_system(number))
+
