63134905a4397190d1e5b38471a8d144962366cc
Nickson Kaigi <nickaigi@gmail.com>
2021-07-06 17:14:33 +0300

    i know how to encrypt

--- arcade_solutions/the_core/cipher_26.py
+++ arcade_solutions/the_core/cipher_26.py
@@ -1,7 +1,21 @@
+import string
+
+
+def encrypt(msg):
+    a = string.ascii_lowercase
+    cipher_text = []
+    s = 0
+    for c in msg:
+        s += a.find(c)
+        cipher_text.append(a[s % 26])
+    return ''.join(cipher_text)
+
+
 def cipher_26(message):
     return ''
 
 
 if __name__ == '__main__':
     message = 'taiaiaertkixquxjnfxxdh'
-    print(cipher_26(message))
+    print(message == encrypt('thisisencryptedmessage'))
+    #print(cipher_26(message))
