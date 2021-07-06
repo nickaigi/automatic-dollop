e16610fa700de044d5bd49b34ac894a63610020b
Nickson Kaigi <nickaigi@gmail.com>
2021-07-06 19:27:46 +0300

    soln

--- arcade_solutions/the_core/cipher_26.py
+++ arcade_solutions/the_core/cipher_26.py
@@ -11,11 +11,16 @@ def encrypt(msg):
     return ''.join(cipher_text)
 
 
-def cipher_26(message):
-    return ''
+def cipher_26(msg):
+    a = string.ascii_lowercase
+    s, n = '', 0
+    for i, c in enumerate(msg):
+        s += a[(a.find(c) - n) % 26]
+        n += (a.find(c) - n) % 26
+    return s
 
 
 if __name__ == '__main__':
     message = 'taiaiaertkixquxjnfxxdh'
     print(message == encrypt('thisisencryptedmessage'))
-    #print(cipher_26(message))
+    print(cipher_26(message))
