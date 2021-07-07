91ebc468b6cd824b1db08aba519964913c3a9c4f
Nickson Kaigi <nickaigi@gmail.com>
2021-07-07 12:58:54 +0300

    soln

--- arcade_solutions/the_core/stolen_lunch.py
+++ arcade_solutions/the_core/stolen_lunch.py
@@ -2,11 +2,9 @@ import string
 
 
 def stolen_lunch(note):
-    s = string.ascii_lowercase
-    res = ''
-    for c in note:
-        if c.isdigit():
-            res += s[int(c)]
-        else:
-            res += c
-    return res
+    tab = str.maketrans('0123456789abcdefghij', 'abcdefghij0123456789')
+    return str(note).translate(tab)
+
+if __name__ == '__main__':
+    note = "you'll n4v4r 6u4ss 8t: cdja"
+    print(stolen_lunch(note))
