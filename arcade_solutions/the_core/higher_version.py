924755f7debc3bad2b6fc23d3173358355d0e025
Nickson Kaigi <nickaigi@gmail.com>
2021-07-08 16:52:27 +0300

    soln

--- arcade_solutions/the_core/higher_version.py
+++ arcade_solutions/the_core/higher_version.py
@@ -1,13 +1,5 @@
-def split_version(v):
-    return [int(i) for i in v.split('.')]
-
-
 def higher_version(v1, v2):
-    for x, y in zip(split_version(v1), split_version(v2)):
-        if x < y:
-            return False
-    return True
-
+    return list(map(int, v1.split('.'))) > list(map(int, v2.split('.'))) 
 
 if __name__ == '__main__':
     v1 = '1.10.2'
