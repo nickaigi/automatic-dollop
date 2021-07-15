803fffc3bc278896d8c5d5378c1937075a0726c9
Nickson Kaigi <nickaigi@gmail.com>
2021-07-15 12:23:01 +0300

    initial import

---
+++ arcade_solutions/the_core/array_conversion.py
@@ -0,0 +1,10 @@
+def array_conversion(arr):
+    res = 0
+    for i, x in enumerate(arr):
+        print(i, x)
+    return 0
+
+
+if __name__ == '__main__':
+    arr = [1, 2, 3, 4, 5, 6, 7, 8]
+    print(array_conversion(arr))
