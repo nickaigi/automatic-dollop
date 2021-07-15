a7f119c2398948591fdb06dd6805da3f295ded3a
Nickson Kaigi <nickaigi@gmail.com>
2021-07-15 22:00:24 +0300

    my soln

--- arcade_solutions/the_core/array_previous_less.py
+++ arcade_solutions/the_core/array_previous_less.py
@@ -1,5 +1,18 @@
+def last_small(arr, current):
+    l = len(arr)
+    smallest = -1
+    if l < 1:
+        return smallest
+    if arr[l - 1] < current:
+        smallest = arr[l - 1]
+    return smallest
+
+
 def array_previous_less(items):
-    return [0]
+    new_arr = []
+    for i in range(len(items)):
+        new_arr.append(last_small(items[:i], items[i]))
+    return new_arr
 
 
 if __name__ == '__main__':
