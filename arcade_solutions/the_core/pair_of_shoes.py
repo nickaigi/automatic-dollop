76d51a48eec77242a7518e2dd613921478e146af
Nickson Kaigi <nickaigi@gmail.com>
2021-07-16 21:55:07 +0300

    testing

--- arcade_solutions/the_core/pair_of_shoes.py
+++ arcade_solutions/the_core/pair_of_shoes.py
@@ -1,5 +1,14 @@
 def pair_of_shoes(shoes):
-    return False
+    shoes_dict = {}
+    for s in shoes:
+        if s[0] in shoes_dict.keys():
+            shoes_dict[s[0]].append(s[1])
+        else:
+            shoes_dict[s[0]] = [s[1]]
+    for p in shoes_dict:
+        if len(shoes_dict[p]) != 2:
+            return False
+    return True
 
 
 if __name__ == '__main__':
