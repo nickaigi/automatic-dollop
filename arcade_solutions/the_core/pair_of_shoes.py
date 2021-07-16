ba22666a35d68f0b69b194fd815f6ba9d1b6f9d2
Nickson Kaigi <nickaigi@gmail.com>
2021-07-16 21:02:55 +0300

    initial import

---
+++ arcade_solutions/the_core/pair_of_shoes.py
@@ -0,0 +1,12 @@
+def pair_of_shoes(shoes):
+    return False
+
+
+if __name__ == '__main__':
+    shoes = [
+        [0, 21], 
+        [1, 23], 
+        [1, 21], 
+        [0, 23]
+    ]
+    print(pair_of_shoes(shoes))
