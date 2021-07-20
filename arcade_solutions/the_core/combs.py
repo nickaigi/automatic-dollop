ea48479303d5f53cf9e0936067c08f7b2f879548
Nickson Kaigi <nickaigi@gmail.com>
2021-07-20 09:48:56 +0300

    working on it

--- arcade_solutions/the_core/combs.py
+++ arcade_solutions/the_core/combs.py
@@ -1,4 +1,23 @@
+def check_for_carry(a1, a2):
+    res = []
+    for x, y in zip(a1, a2):
+        if x == 1 and y == 1:
+            # carry
+            pass
+        else:
+            pass
+    return res
+
+
+def gen_array(s):
+    return [1 if c == '*' else 0 for c in s]
+
+
 def combs(c1, c2):
+    c1 = gen_array(c1)
+    c2 = gen_array(c2)
+    for x, y in zip(c1, c2):
+        print(x, y)
     return 0
 
 
