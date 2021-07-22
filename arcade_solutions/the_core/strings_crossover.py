af6de39cf9a236a9dd3d17b88df9499ad945b724
Nickson Kaigi <nickaigi@gmail.com>
2021-07-22 06:13:41 +0300

    using itertools

--- arcade_solutions/the_core/strings_crossover.py
+++ arcade_solutions/the_core/strings_crossover.py
@@ -1,4 +1,14 @@
+import itertools
+
+
 def strings_crossover(arr, result):
+    count = 0
+    for (a, b) in itertools.combinations(arr, 2):
+        count += all(c in a[i] + b[i] for i, c in enumerate(result))
+    return count
+
+
+def strings_crossover_old(arr, result):
     count = 0
     for i in range(len(arr)):
         for j in range(i + 1, len(arr)):
