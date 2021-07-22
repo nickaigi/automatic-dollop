1c317670be7aa2531c0c71100665af5d6a375017
Nickson Kaigi <nickaigi@gmail.com>
2021-07-22 06:09:20 +0300

    initial soln

--- arcade_solutions/the_core/strings_crossover.py
+++ arcade_solutions/the_core/strings_crossover.py
@@ -1,5 +1,16 @@
 def strings_crossover(arr, result):
-    return 0
+    count = 0
+    for i in range(len(arr)):
+        for j in range(i + 1, len(arr)):
+            if check(arr[i], arr[j], result):
+                count += 1
+    return count
+
+def check(a, b, res):
+    for i , j, k in zip(a, b, res):
+        if i != k and j != k:
+            return False
+    return True
 
 
 if __name__ == '__main__':
