e5addc3cd8d67077996160171306f9c291b8895b
Nickson Kaigi <nickaigi@gmail.com>
2021-07-22 05:14:23 +0300

    initial import

---
+++ arcade_solutions/the_core/strings_crossover.py
@@ -0,0 +1,8 @@
+def strings_crossover(arr, result):
+    return 0
+
+
+if __name__ == '__main__':
+    arr = ['abc', 'aaa', 'aba', 'bab']
+    result = 'bbb'
+    print(strings_crossover(arr, result))
