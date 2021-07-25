f822df54da3923e77ced2db4e8f7f3372a1a2951
Nickson Kaigi <nickaigi@gmail.com>
2021-07-25 16:52:32 +0300

    soln

--- arcade_solutions/the_core/cyclic_string.py
+++ arcade_solutions/the_core/cyclic_string.py
@@ -1,5 +1,9 @@
 def cyclic_string(s):
-    return 0
+    for i in range(0, len(s)):
+        sub = s[0:i+1]
+        multiplier = len(s) // (i+1) + 1
+        if s in sub * multiplier:
+            return i + 1
 
 
 if __name__ == '__main__':
