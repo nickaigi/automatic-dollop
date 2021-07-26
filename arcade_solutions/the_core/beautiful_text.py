eb5c9ecb0175569c2a82eedc93cb0910eb64ce3a
Nickson Kaigi <nickaigi@gmail.com>
2021-07-26 18:42:20 +0300

    initial import

---
+++ arcade_solutions/the_core/beautiful_text.py
@@ -0,0 +1,9 @@
+def beautiful_text(s, l, r):
+    return False
+
+
+if __name__ == '__main__':
+    s = 'Look at this example of a correct text'
+    l = 5
+    r = 15
+    print(beautiful_text(s, l, r))
