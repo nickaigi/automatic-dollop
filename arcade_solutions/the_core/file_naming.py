c65051dfc9cef852fda2123fbe70ec6494675976
Nickson Kaigi <nickaigi@gmail.com>
2021-08-02 12:26:07 +0300

    soln

--- arcade_solutions/the_core/file_naming.py
+++ arcade_solutions/the_core/file_naming.py
@@ -1,5 +1,13 @@
 def file_naming(names):
-    pass
+    result = []
+    for name in names:
+        if name in result:
+            count = 1
+            while f'{name}({count})' in result:
+                count += 1
+            name = f'{name}({count})'
+        result.append(name)
+    return result
 
 
 if __name__ == '__main__':
