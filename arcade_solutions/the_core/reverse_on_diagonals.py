c234f7d2ba37091e034c64d59e16509b1b105899
Nickson Kaigi <nickaigi@gmail.com>
2021-08-05 21:20:14 +0300

    made changes

--- arcade_solutions/the_core/reverse_on_diagonals.py
+++ arcade_solutions/the_core/reverse_on_diagonals.py
@@ -1,15 +1,14 @@
 def reverse_on_diagonals(matrix):
-    forward_counter = 0
-    reverse_counter = len(matrix) - 1 
-    first_diagonal = []
-    second_diagonal = []
-    for row in matrix:
-        first_diagonal.append(row[forward_counter])
-        second_diagonal.append(row[reverse_counter])
-        forward_counter += 1
-        reverse_counter -= 1
-    return first_diagonal, second_diagonal
+    size = len(matrix) - 1
 
+    first_diag = []
+    second_diag = []
+
+    for i in range(len(matrix)):
+        first_diag.append(matrix[i][i])
+        second_diag.append(matrix[i][size])
+        size -= 1
+    return first_diag, second_diag
 
 if __name__ == '__main__':
     matrix = [
