41306a728571d081d6474bfc6bc91402262a572e
Nickson Kaigi <nickaigi@gmail.com>
2021-08-05 20:39:00 +0300

    i have the first and second diagonal

--- arcade_solutions/the_core/reverse_on_diagonals.py
+++ arcade_solutions/the_core/reverse_on_diagonals.py
@@ -1,5 +1,14 @@
 def reverse_on_diagonals(matrix):
-    pass
+    forward_counter = 0
+    reverse_counter = len(matrix) - 1 
+    first_diagonal = []
+    second_diagonal = []
+    for row in matrix:
+        first_diagonal.append(row[forward_counter])
+        second_diagonal.append(row[reverse_counter])
+        forward_counter += 1
+        reverse_counter -= 1
+    return first_diagonal, second_diagonal
 
 
 if __name__ == '__main__':
