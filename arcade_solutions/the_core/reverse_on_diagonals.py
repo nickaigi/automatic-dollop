bb34a5d79a7c027ccd2a6aaf883f24d85d83030d
Nickson Kaigi <nickaigi@gmail.com>
2021-08-05 19:07:06 +0300

    initial import

---
+++ arcade_solutions/the_core/reverse_on_diagonals.py
@@ -0,0 +1,12 @@
+def reverse_on_diagonals(matrix):
+    pass
+
+
+if __name__ == '__main__':
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+
+    print(reverse_on_diagonals(matrix))
