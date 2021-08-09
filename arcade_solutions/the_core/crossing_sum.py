7012908441ff799e35fed6d07a583d6455439e20
Nickson Kaigi <nickaigi@gmail.com>
2021-08-09 19:55:20 +0300

    initial import

---
+++ arcade_solutions/the_core/crossing_sum.py
@@ -0,0 +1,13 @@
+def crossing_sum(m, a, b):
+    pass
+
+
+if __name__ == '__main__':
+    m = [
+        [1, 1, 1, 1], 
+        [2, 2, 2, 2], 
+        [3, 3, 3, 3]
+    ]
+    a = 1
+    b = 3
+    print(crossing_sum(m, a, b))
