f53515f1c186567b761f2fb8bf6f41620f08df43
Nickson Kaigi <nickaigi@gmail.com>
2021-08-10 21:14:52 +0300

    soln

--- arcade_solutions/the_core/draw_rectangle.py
+++ arcade_solutions/the_core/draw_rectangle.py
@@ -1,4 +1,23 @@
 def draw_rectangle(canvas, rectangle):
+    (x1, y1, x2, y2) = rectangle
+
+    canvas[y1][x1] = '*'
+    canvas[y1][x2] = '*'
+    canvas[y2][x1] = '*'
+    canvas[y2][x2] = '*'
+
+    for x in range(x1+1, x2):
+        canvas[y1][x] = '-'
+        canvas[y2][x] = '-'
+
+    for y in range(y1+1, y2):
+        canvas[y][x1] = '|'
+        canvas[y][x2] = '|'
+
+    return canvas
+
+
+def draw_rectangle_old(canvas, rectangle):
     a1, b2 = rectangle[:2], rectangle[2:]
     a2, b1 = [b2[0], a1[1]], [a1[0],b2[1]]
 
