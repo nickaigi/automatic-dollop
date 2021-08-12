8783fd2664a6615f2250cd3c1ece8f13f83428a4
Nickson Kaigi <nickaigi@gmail.com>
2021-08-12 21:56:33 +0300

    soln

--- arcade_solutions/the_core/volleyball_positions.py
+++ arcade_solutions/the_core/volleyball_positions.py
@@ -1,4 +1,11 @@
 def volleyball_positions(formation, k):
+
+    def reverse_rotate(b):
+        b[3][2], b[2][1], b[3][0], b[1][0], b[0][1], b[1][2] = b[2][1], b[3][0], b[1][0], b[0][1], b[1][2], b[3][2]
+
+    for _ in range(k % 6):
+        reverse_rotate(formation)
+
     return formation
 
 
